a8fc47f7e8c8a9421f0b8f4b3d9e24c21c444845
jinyoung03 <58543322+jinyoung03@users.noreply.github.com>
2020-01-20 09:37:54 +0900

    Delete TestDAO.java

--- src/main/java/kr/withanimal/member/dao/TestDAO.java
+++
@@ -1,9 +0,0 @@
-package kr.withanimal.member.dao;
-
-import java.util.HashMap;
-
-public interface TestDAO {
-	public String today();
-	public int mul(HashMap<String, Integer> map);
-	public int sum(HashMap<String, Integer> map);
-}
